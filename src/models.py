147720be764533b130db9b87b71c1369c445e49c
GorgGoddess <jackson.anna1220@gmail.com>
2023-03-31 15:32:38 +0000

    changes

--- src/models.py
+++ src/models.py
@@ -13,7 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     post = relationship ("Post", backref = 'user', lazy=True)
-    
+    follower = relationship ("Follower", backref = 'user', lazy=True)
 
 class Post(Base):
     __tablename__ = 'post'
